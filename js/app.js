94b0a8473da4108508cf64a0952510ba274ac354
JPS Archives <joydipbanik6@gmail.com>
2023-08-20 12:45:50 +0530

    update discount apply and grand total

--- js/app.js
+++ js/app.js
@@ -8,29 +8,46 @@ function item(target) {
   p.innerText = `${itemCount + 1}. ${itemName}`;
   itemNameList.appendChild(p);
 
-  const itemPriceString = target.childNodes[5].childNodes[3].childNodes[0].innerText;
+  const itemPriceString =
+    target.childNodes[5].childNodes[3].childNodes[0].innerText;
   const itemPrice = parseFloat(itemPriceString);
 
   const totalPrice = document.getElementById("total-price");
   const totalPriceString = totalPrice.innerText;
   const totalPriceNumber = parseFloat(totalPriceString);
+  const totalCalculate = itemPrice + totalPriceNumber;
+  totalPrice.innerText = totalCalculate.toFixed(2);
 
-  const calculate = itemPrice + totalPriceNumber;
-  totalPrice.innerText = calculate;
+  const grandPrice = document.getElementById("grand-price");
+  const grandPriceString = grandPrice.innerText;
+  const grandPriceNumber = parseFloat(grandPriceString);
+  const grandCalculate = itemPrice + grandPriceNumber;
+  grandPrice.innerText = grandCalculate.toFixed(2);
 
   const purchaseBtn = document.getElementById("purchase-btn");
-  if (parseFloat(calculate) >= 200) {
-      purchaseBtn.removeAttribute("disabled");
-      purchaseBtn.classList.remove("bg-gray-300");
-      purchaseBtn.classList.add("bg-[#E527B2]");
+  if (parseFloat(totalCalculate) >= 200) {
+    purchaseBtn.removeAttribute("disabled");
+    purchaseBtn.classList.remove("bg-gray-300");
+    purchaseBtn.classList.add("bg-[#E527B2]");
   } else {
-      purchaseBtn.setAttribute("disabled", true);
-      purchaseBtn.classList.add("bg-gray-300");
+    purchaseBtn.setAttribute("disabled", true);
+    purchaseBtn.classList.add("bg-gray-300");
   }
+
+  document
+    .getElementById("discount-apply")
+    .addEventListener("click", function () {
+      const discountCheck = (grandCalculate / 100) * 20;
+      const afterDiscount = grandCalculate - discountCheck;
+      // console.log(disCheck);
+      document.getElementById("discount-price").innerText = discountCheck.toFixed(2);
+      document.getElementById("grand-price").innerText =
+        afterDiscount.toFixed(2);
+    });
 }
 
 document.getElementById("discount").addEventListener("keyup", function (e) {
-  const discountBtn = document.getElementById("disApply");
+  const discountBtn = document.getElementById("discount-apply");
   const text = e.target.value;
   if (text === "SELL200") {
     discountBtn.removeAttribute("disabled");
@@ -41,17 +58,3 @@ document.getElementById("discount").addEventListener("keyup", function (e) {
     discountBtn.classList.add("bg-gray-300");
   }
 });
-
-const discountInputValue = document
-  .getElementById("disApply")
-  .addEventListener("click", function () {
-    (500 * 100) / 20;
-  });
-
-console.log(discountInputValue);
-//   function disApply(calValue) {
-//       (calValue * 100) / 20;
-//       return disApply;
-// }
-
-console.log(disApply(500));
